e72668a8948dad53fbacaf3b0e6df5464d13ec20
frankyhbyb <frankyhbyb@163.com>
2010-05-01 10:38:12 +0000

    统计分析

---
+++ OnLineExam/OnLineExam/Web/QuestionAnalysis.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.IO;
+using System.Data.OleDb;
+using localhost;
+
+public partial class Web_QuestionAnalysis : System.Web.UI.Page
+{
+    BLWS_QuestionAnalysis service = new BLWS_QuestionAnalysis();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.Page.Title = "问题统计分析";
+      /* if (!IsPostBack)
+        {
+            if (Session["userID"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                string userId = Session["userID"].ToString();
+                string userName = userService.GetUserName(userId);
+                Label i1 = (Label)Page.Master.FindControl("labUser");
+                i1.Text = userName;
+            }
+        }*/
+        updatescore();
+    }
+
+    public void updatescore()
+    {
+        //GridView1.Columns.
+        string StudentID = DropDownList1.SelectedValue;
+        string[] s = service.GetList(StudentID);
+        DataTable table = new DataTable();
+        table.Columns.Add("题目序号");
+        table.Columns.Add("课程序号");
+        table.Columns.Add("题目");
+        int j = 0;
+        while (s[j]!=null)
+        {
+            DataRow row = table.NewRow();
+            row[0] = s[j];
+            row[1] = s[++j];
+            row[2] = s[++j];
+            j++;
+            table.Rows.Add(row);
+        } 
+        GridView1.DataSource = table;
+        GridView1.DataBind();
+    }
+
+
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+      /*  if (e.Row.RowType == DataControlRowType.DataRow || e.Row.RowType == DataControlRowType.Separator)
+        {
+            Label label1 = e.Row.FindControl("Label1") as Label;
+            label1.Text = (e.Row.RowIndex + 1).ToString();
+        }*/
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            e.Row.Attributes.Add("onmouseover", "this.style.backgroundColor='#cbe2fa'");
+            e.Row.Attributes.Add("onmouseout", "this.style.backgroundColor='#FFFFFF'");
+        }
+    }
+    protected void ddlCourse_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        updatescore();
+    }
+}
---
+++ OnLineExam/OnLineExamDALWS/DALWS_QuestionAnalysis.asmx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+using OnLineExamModel;
+using System.ComponentModel;
+
+namespace OnLineExamDALWS
+{
+    /// <summary>
+    /// DALWS_QuestionAnalysis 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
+    // [System.Web.Script.Services.ScriptService]
+    public class DALWS_QuestionAnalysis : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<FillBlankProblem> GeFillBlankProblemList()
+        {
+            using (SqlConnection con = DBHelp.GetConnection())
+            {
+                string sql = "select * from FillBlankProblem";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                List<FillBlankProblem> list = new List<FillBlankProblem>();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    FillBlankProblem fill = new FillBlankProblem();
+                    fill.ID = Convert.ToInt32(dr["ID"]);
+                    fill.CourseID = Convert.ToInt32(dr["CourseID"]);
+                    fill.FrontTitle = dr["FrontTitle"].ToString();
+                    list.Add(fill);
+                }
+                return list;
+            }
+        }
+
+
+
+        [WebMethod]
+        public List<SingleProblem> GetSingleProblemList()
+        {
+            using (SqlConnection con = DBHelp.GetConnection())
+            {
+                string sql = "select * from SingleProblem";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                List<SingleProblem> list = new List<SingleProblem>();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    SingleProblem sing = new SingleProblem();
+                    sing.ID = Convert.ToInt32(dr["ID"]);
+                    sing.CourseID = Convert.ToInt32(dr["CourseID"]);
+                    sing.Title = dr["Title"].ToString();
+                    list.Add(sing);
+                }
+                return list;
+            }
+        }
+
+
+
+        [WebMethod]
+        public List<QuestionProblem> GetQuestionProblemList()
+        {
+            using (SqlConnection con = DBHelp.GetConnection())
+            {
+                string sql = "Select * from QuestionProblem";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                List<QuestionProblem> list = new List<QuestionProblem>();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    QuestionProblem ques = new QuestionProblem();
+                    ques.ID = Convert.ToInt32(dr["ID"]);
+                    ques.CourseID = Convert.ToInt32(dr["CourseID"]);
+                    ques.Title = dr["Title"].ToString();
+                    list.Add(ques);
+                }
+                return list;
+            }
+        }
+
+        [WebMethod]
+        public List<MultiProblem> GetMultiProblemList()
+        {
+            using (SqlConnection con = DBHelp.GetConnection())
+            {
+                string sql = "select * from MultiProblem";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                List<MultiProblem> list = new List<MultiProblem>();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    MultiProblem mul = new MultiProblem();
+                    mul.ID = Convert.ToInt32(dr["ID"]);
+                    mul.CourseID = Convert.ToInt32(dr["CourseID"]);
+                    mul.Title = dr["Title"].ToString();
+                    list.Add(mul);
+                }
+                return list;
+            }
+        }
+
+
+        [WebMethod]
+        public List<JudgeProblem> GetJudgeProblemList()
+        {
+            using (SqlConnection conn = DBHelp.GetConnection())
+            {
+                string sql = "select * from JudgeProblem";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                List<JudgeProblem> list = new List<JudgeProblem>();
+                SqlDataReader DR = cmd.ExecuteReader();
+                if (DR.Read())
+                {
+                    JudgeProblem judge = new JudgeProblem();
+                    judge.ID = Convert.ToInt32(DR["ID"]);
+                    judge.CourseID = Convert.ToInt32(DR["CourseID"]);
+                    judge.Title = DR["Title"].ToString();
+                    list.Add(judge);
+                }
+                return list;
+            }
+        }
+
+     
+
+
+    }
+}
