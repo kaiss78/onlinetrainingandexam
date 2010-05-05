36e7b764b41fabf3adc4e1443892c4617187d34b
frankyhbyb@163.com <frankyhbyb@163.com@33eecce9-c8c0-531a-0014-9a625da8c4b3>
2010-05-05 02:27:21 +0000

    管理员和教师登陆界面不一样，教师无权使用数据库恢复，用户管理等功能

--- trunk/OnLineExam/OnLineExam/MasterPage.master.cs
+++ trunk/OnLineExam/OnLineExam/MasterPage.master.cs
@@ -8,12 +8,38 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using localhost;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
-
+    BLLWS_Role roleService = new BLLWS_Role();
     protected void LinkButton1_Click(object sender, EventArgs e)
     {
         Response.Redirect("Login.aspx");
+    }
+
+    public int RedirectPage()
+    {
+        if (Session["userID"] == null)
+            Response.Redirect("Login.aspx");
+
+        Role user = new Role();
+        string userId = Session["userID"].ToString();
+        user.RoleName = roleService.GetRoleName(userId);
+        switch (user.RoleName)
+        {
+            case "管理员":
+                return 0;
+                break;
+            case "教师":
+                return 1;
+                break;
+            case "学生":
+                return 2;
+                break;
+            default:
+                break;
+        }
+        return -1;
     }
 }
